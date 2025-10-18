615f173656268939ebc4f164e685a7e48289b683
pankajThakur1874 <pankajthakur1874@gmail.com>
2025-10-18 09:05:27 +0530

    fix: correct base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import path from 'path'
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '')
 
   return {
-    base: '/', // 👈 REQUIRED for GitHub Pages
+    // 👇👇 THIS IS CRITICAL 👇👇
+    base: '/pkpharma/',
 
     server: {
       port: 3000,
